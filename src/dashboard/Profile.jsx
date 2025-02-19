de8472a628cce43c2a759455821849ab9bfba383
Fatema Tabassum Diba <fatematabassum00700@gmail.com>
2025-02-19 19:41:41 +0600

    Profile data

--- src/dashboard/Profile.jsx
+++ src/dashboard/Profile.jsx
@@ -1,24 +1,41 @@
 import { FaUserCircle } from "react-icons/fa";
+import useUser from "../hooks/useUser";
+import Title from "../components/Title";
 
 const Profile = () => {
+  const [userData, userLoading] = useUser();
+  console.log(userData);
   return (
-    <section className="py-16 px-6 flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-xl flex items-center">
-        <div className="w-1/3 flex justify-center">
-          <FaUserCircle className="text-gray-500 text-8xl" />
-        </div>
-        <div className="w-2/3 text-left pl-6">
-          <h2 className="text-3xl font-bold text-gray-800">John Doe</h2>
-          <p className="text-lg text-gray-600 mt-2">Software Developer</p>
-          <p className="text-gray-500 mt-2">johndoe@example.com</p>
-          <div className="mt-6">
-            <button className="px-6 py-2 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition">
-              Edit Profile
-            </button>
+    <div>
+      <Title title="Profile"></Title>
+      <section className="py-16 px-6 flex justify-center items-center">
+        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-3xl flex items-center">
+          {userData?.photo ? (
+            <img
+              src={userData.photo}
+              alt="Profile"
+              className="w-32 h-32 rounded-full shadow-lg border border-gray-300"
+            />
+          ) : (
+            <FaUserCircle className="text-gray-400 text-8xl" />
+          )}
+          <div className="w-2/3 text-left pl-6">
+            <h2 className="text-3xl font-bold text-gray-800">
+              {userData?.displayName}
+            </h2>
+            <p className="text-lg text-gray-600 mt-2">
+              Role: {userData?.userType}
+            </p>
+            <p className="text-gray-500 mt-2">{userData?.email}</p>
+            <div className="mt-6">
+              <button className="px-6 py-2 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition">
+                Edit Profile
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
